28c06b0f0c9d7640acc3e0363e277e6ee92387b5
Adan <programmerAdan@gmail.com>
2019-02-27 21:22:57 -0600

    Starting Command Pattern
    Working on getting this setup.

---
+++ Assets/Scripts/Player/Commands/Command.cs
@@ -0,0 +1,13 @@
+﻿//main class where other Commands can inherit from
+public abstract class Command
+{
+    //used to Execute action associated with it's command
+    public abstract void Execute();
+    
+    //used to Undo the action associated with it's command
+    public abstract void UnExecute();
+    
+    //Logs which command has run
+    public abstract string Log();
+
+}
---
+++ Assets/Scripts/Player/Commands/Move Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+
+public enum MoveDirection { MovePosX, MoveNegX, MovePosY, MoveNegY, MovePosZ, MoveNegZ }
+
+namespace Assets.Scripts.Player.Commands
+{
+
+    public class Move_Command : Command
+    {
+        protected float moveDistance = 1.0f;
+        protected MoveDirection direction;
+        protected Rigidbody rgbd;
+        protected Vector3 force;
+
+        public Move_Command(Vector3 f, Rigidbody rb, MoveDirection md)
+        {
+            force = f;
+            rgbd = rb;
+            direction = md;
+        }
+
+        public override void Execute()
+        {
+            rgbd.AddForce(force, ForceMode.VelocityChange);
+
+        }
+
+        public override void UnExecute()
+        {
+            rgbd.AddForce(-force, ForceMode.VelocityChange);
+        }
+
+        public override string Log()
+        {
+            return $"{this.GetType()} has been called, force: {this.force}, direction: {direction.ToString()}";
+        }
+    }
+}

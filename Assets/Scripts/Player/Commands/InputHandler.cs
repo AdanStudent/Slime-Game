a03cda229c448a2ae985b7a711170d02c8d376ca
Adan <programmerAdan@gmail.com>
2019-02-27 22:05:36 -0600

    Input Handler

--- Assets/Scripts/Player/Commands/InputHandler.cs
+++ Assets/Scripts/Player/Commands/InputHandler.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Player.Commands;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    //keeping reference of all the Move Commands that the player is calling 
+    Stack<Move_Command> moves;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moves = new Stack<Move_Command>();
     }
 
     // Update is called once per frame

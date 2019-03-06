04759644d617034ec9d4391d6e9bf31e320d69d3
Tess Olson <37564977+eleanorpixie2@users.noreply.github.com>
2019-03-06 14:48:14 -0600

    Added element interaction

---
+++ Assets/Scripts/Elements'/ElementEnum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementEnum : MonoBehaviour
+{
+    public enum Elements {
+        Fire,
+        Grass,
+        Ash,
+        Water,
+        Cheese,
+        None
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

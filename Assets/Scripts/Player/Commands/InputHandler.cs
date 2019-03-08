f345683e4a659cddd1c38e29e560e2fe156fd536
BrieAnna <BrieAnna.Allen@gmail.com>
2019-03-08 15:39:33 -0600

    UpdatedNetworking

--- Assets/Scripts/Player/Commands/InputHandler.cs
+++ Assets/Scripts/Player/Commands/InputHandler.cs
@@ -3,19 +3,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class InputHandler : MonoBehaviour
+public class InputHandler : NetworkBehaviour
 {
     //keeping reference of all the Move Commands that the player is calling 
     Stack<Move_Command> moves;
-    public Rigidbody player;
+    public Rigidbody playerb;
+    public GameObject player;
     public float Speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         moves = new Stack<Move_Command>();
-        player.freezeRotation = true;
+        playerb = this.gameObject.GetComponent<Rigidbody>();
+        player = playerb.gameObject;
+        playerb.freezeRotation = true;
     }
 
     bool undo;
@@ -31,6 +35,10 @@ public class InputHandler : MonoBehaviour
     private float turnSmoothTime = 0.1f;
     private void MouseInput()
     {
+        if (hasAuthority == false)
+        {
+            return;
+        }
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector2 inputDir = input.normalized;
 
@@ -38,11 +46,11 @@ public class InputHandler : MonoBehaviour
         {
             Debug.Log("This will need to be refactored into Command Pattern");
             float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
-            player.transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(player.transform.eulerAngles.y, targetRotation, ref turnSmoothVel, turnSmoothTime);
+            playerb.transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(playerb.transform.eulerAngles.y, targetRotation, ref turnSmoothVel, turnSmoothTime);
 
         }
         float targetSpeed = inputDir.magnitude * Speed;
-        player.transform.Translate(player.transform.forward * targetSpeed * Time.deltaTime, Space.World);
+        playerb.transform.Translate(playerb.transform.forward * targetSpeed * Time.deltaTime, Space.World);
     }
 
     private void CallReplay()
@@ -72,23 +80,23 @@ public class InputHandler : MonoBehaviour
 
         //if (inputX > 0)
         //{
-        //    movementCommand = new Move_Command(new Vector3(1, 0), player, MoveDirection.MovePosX);
+        //    movementCommand = new Move_Command(new Vector3(1, 0), playerb, MoveDirection.MovePosX);
         //    moves.Push(movementCommand);
             
         //}
         //if (inputX < 0)
         //{
-        //    movementCommand = new Move_Command(new Vector3(-1, 0), player, MoveDirection.MoveNegX);
+        //    movementCommand = new Move_Command(new Vector3(-1, 0), playerb, MoveDirection.MoveNegX);
         //    moves.Push(movementCommand);
         //}
         //if (inputY > 0)
         //{
-        //    movementCommand = new Move_Command(new Vector3(0, 0, 1), player, MoveDirection.MovePosZ);
+        //    movementCommand = new Move_Command(new Vector3(0, 0, 1), playerb, MoveDirection.MovePosZ);
         //    moves.Push(movementCommand);
         //}
         //if (inputY < 0)
         //{
-        //    movementCommand = new Move_Command(new Vector3(0, 0, -1), player, MoveDirection.MoveNegZ);
+        //    movementCommand = new Move_Command(new Vector3(0, 0, -1), playerb, MoveDirection.MoveNegZ);
         //    moves.Push(movementCommand);
         //}
     }
